c7e297f5beb8236d8c1a43b35d8fe61d7c0d60a4
Alvaro Martinez <alvaromfdv@gmail.com>
2023-06-13 13:40:17 +0200

    test

--- models/User.model.js
+++ models/User.model.js
@@ -18,6 +18,10 @@ const userSchema = new Schema(
       type: String,
       required: [true, "Name is required."],
     },
+    firstName:{
+      type: String,
+      required: [true, "First Name is required."],
+    }
   },
   {
     // this second object adds extra properties: `createdAt` and `updatedAt`
