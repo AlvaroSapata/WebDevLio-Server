2025fc5ebc4197c982aec38094c154054907954e
Jonatan Iglesias <jonathan-18719@outlook.es>
2023-06-13 19:54:33 +0200

    cloudinary done

--- routes/uploader.routes.js
+++ routes/uploader.routes.js
@@ -1,5 +1,18 @@
 const express = require("express");
 const router = express.Router();
+const imageUploader = require("../middleware/cloudinaryImage.config")
+
+// POST "/api/upload/image"
+router.post("/image", imageUploader.single("image"), (req, res, next) => {
+  
+    if (!req.file) {
+      next("No image uploaded!");
+      return;
+    }
+  
+    
+    res.json({ imageUrl: req.file.path });
+  });
 
 
 module.exports = router;
