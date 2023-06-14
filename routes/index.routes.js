5d1efed36cf0a7d80430df5598266f564ecab33f
Alvaro Martinez <alvaromfdv@gmail.com>
2023-06-14 12:49:52 +0200

    login/signup

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -5,12 +5,18 @@ router.get("/", (req, res, next) => {
   res.json("All good in here");
 });
 
+// auth routes
+router.use("/auth", require("./auth.routes"));
+
 // project routes
 router.use("/", require("./projects.routes"))
+
 // contact routes
 router.use("/", require("./contact.routes"))
+
 // skillsList routes
 router.use("/", require("./skillsList.routes"))
+
 // homeProfile routes
 router.use("/", require("./homeProfile.routes"))
 
